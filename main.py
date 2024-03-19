bbcba41a9243a68cea0643a346f9b26c8ed1505f
jiandanhuoduanqiao95 <133885726+jiandanhuoduanqiao95@users.noreply.github.com>
2024-03-19 10:32:12 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -257,22 +257,6 @@ def user_interface():
     else:
         print("无效的选择，退出程序。")
 
-#代码建表
-def create_table(conn,table_name):
-    cursor=conn.cursor()
-    create_table_query = f"""
-                CREATE TABLE IF NOT EXISTS {table_name} (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    username VARCHAR(255) NOT NULL,
-                    password VARCHAR(255) NOT NULL,
-                    is_admin TINYINT(1) NOT NULL DEFAULT 0
-                ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-    
-    )
-    """
-    cursor.execute(create_table_query)
-    cursor.close()
-
     # 主程序
 if __name__ == '__main__':
     user_interface()
